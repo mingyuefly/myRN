c8c853a1769437db4f7b4071e57e33f6656fbc0a
guomingyue <mingyuefly@qq.com>
2019-08-22 19:13:15 +0800

    react-navigation页面切换

--- MyRNProject/App.js
+++ MyRNProject/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button} from 'react-native';
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 
 class HomeScreen extends React.Component {
@@ -7,6 +7,7 @@ class HomeScreen extends React.Component {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>Home Screen</Text>
+        <Button title = 'Go to Details' onPress = {()=>this.props.navigation.navigate('Details')} />
       </View>
     );
   }
