757316f62bccb71e0dc1143f68a30df269be5b29
guomingyue <mingyuefly@qq.com>
2019-08-23 10:25:06 +0800

    使用push反复添加同一个页面

--- MyRNProject/App.js
+++ MyRNProject/App.js
@@ -13,11 +13,13 @@ class HomeScreen extends React.Component {
   }
 }
 
+// 从DetailsScreen继续进入DetailsScreen页面，如果使用navigate啥也不会做，因为此时已经在Details这个路由上了，如果要继续添加这个页面，则需要是用push方法
 class DetailsScreen extends React.Component {
   render() {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>Details Screen</Text>
+        <Button title = 'Go to Details...again' onPress = {()=>this.props.navigation.push('Details')} />
       </View>
     );
   }
