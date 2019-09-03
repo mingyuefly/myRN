6d959b93198fd8a23513d05315b37a5400267779
guomingyue <mingyuefly@qq.com>
2019-09-03 18:40:10 +0800

    设置导航栏标题，传参设置导航栏，动态修改导航栏，设置导航栏样式

--- MyRNProject/App.js
+++ MyRNProject/App.js
@@ -4,6 +4,16 @@ import { createStackNavigator, createAppContainer, createBottomTabNavigator } fr
 //import console = require('console');
 
 class HomeScreen extends React.Component {
+  static navigationOptions = {
+    title:'Home',
+    headerStyle:{
+      backgroundColor:'#f45113',
+    },
+    headerTintColor:'#fff',
+    headerTitleStyle:{
+      fontWeight:'bold'
+    },
+  };
   render() {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
@@ -15,6 +25,11 @@ class HomeScreen extends React.Component {
 }
 
 class DetailsScreen extends React.Component {
+  static navigationOptions = ({navigation}) => {
+    return {
+      title:navigation.getParam('otherParam', 'A Nested Details Screen'),
+    };
+  };
   render() {
     const {navigation} = this.props;
     const itemId = navigation.getParam('itemId', 'NO-ID');
@@ -25,6 +40,7 @@ class DetailsScreen extends React.Component {
         <Text>ItemId:{JSON.stringify(itemId)}</Text>
         <Text>otherParam:{JSON.stringify(otherParam)}</Text>
         <Button title = 'Go to Details...again' onPress = {()=>this.props.navigation.push('Details', {itemId:Math.floor(Math.random() * 100)})} />
+        <Button title = 'Update the title' onPress = {() => this.props.navigation.setParams({otherParam:'Updated!'})} />
         <Button title = 'Go to home' onPress = {() => this.props.navigation.navigate('Home')} />
         <Button title = 'Go back' onPress = {() => this.props.navigation.goBack()} />
       </View>
